763596f2c57f8dfa2541f0943dc2928db84b441f
smebes <sm01ebes@gmail.com>
2025-06-12 00:09:42 +0300

    Webview bileşenleri eklendi; pubspec.yaml dosyasına webview_flutter ve webview_flutter_web bağımlılıkları eklendi. Görsel görüntüleyici sayfasında web desteği için güncellemeler yapıldı ve macOS için gerekli webview kaydı gerçekleştirildi.

--- lib/pages/image_viewer_page.dart
+++ lib/pages/image_viewer_page.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
+// Web için ek importlar
+import 'dart:ui' as ui;
+import 'dart:html' as html;
 
 class ImageViewerPage extends StatelessWidget {
   final String? url;
@@ -6,12 +10,30 @@ class ImageViewerPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Görsel Görüntüleyici')),
-      body:
-          url == null
-              ? const Center(child: Text('Görsel bulunamadı'))
-              : InteractiveViewer(child: Center(child: Image.network(Uri.encodeFull(url!)))),
-    );
+    if (kIsWeb && url != null) {
+      final viewID = 'iframe_${url.hashCode}';
+      // ignore: undefined_prefixed_name
+      ui.platformViewRegistry.registerViewFactory(
+        viewID,
+        (int viewId) =>
+            html.IFrameElement()
+              ..src = url!
+              ..style.border = 'none'
+              ..width = '100%'
+              ..height = '100%',
+      );
+      return Scaffold(
+        appBar: AppBar(title: const Text('Görsel Görüntüleyici')),
+        body: HtmlElementView(viewType: viewID),
+      );
+    } else {
+      return Scaffold(
+        appBar: AppBar(title: const Text('Görsel Görüntüleyici')),
+        body:
+            url == null
+                ? const Center(child: Text('Görsel bulunamadı'))
+                : InteractiveViewer(child: Center(child: Image.network(url!))),
+      );
+    }
   }
 }
